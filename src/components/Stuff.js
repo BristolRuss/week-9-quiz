87ce2ba708815c643ed029b5dd6a36289f641eb0
BristolRuss <BristolRuss@gmail.com>
2020-03-06 18:58:16 +0000

    adding updated files

--- src/components/Stuff.js
+++ src/components/Stuff.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {HashRouter as Router, Route, Switch} from "react-router-dom";
 import Multiplier from './Multiplier'
 import EvenClicks from './EvenClicks';
 import CountBy from './CountBy';
